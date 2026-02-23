6309f132008c00a8012a7c45466df9016a0963f2
youqiq <shsbshwbs3@gmail.com>
2026-02-23 14:24:55 +0700

    Update build.gradle.kts

--- KuramanimeProvider/build.gradle.kts
+++ KuramanimeProvider/build.gradle.kts
@@ -1,5 +1,5 @@
 // use an integer for version numbers
-version = 46
+version = 47
 
 
 cloudstream {
@@ -7,7 +7,7 @@ cloudstream {
     // All of these properties are optional, you can safely remove them
 
     // description = "Lorem Ipsum"
-     authors = listOf("Hexated")
+     authors = listOf("youqiq")
 
     /**
      * Status int as the following:
